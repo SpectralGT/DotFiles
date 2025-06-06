243aeae89435ab41525609aa3ad17f3c505f2704
SpectralGT <atharvsingh1396@gmail.com>
2025-06-06 16:14:56 +0530

    added chadwm

---
+++ chadwm/chadwm/themes/onedark.h
@@ -0,0 +1,12 @@
+static const char black[]       = "#000000";
+static const char white[]       = "#abb2bf";
+static const char gray2[]       = "#000000"; // unfocused window border
+static const char gray3[]       = "#ffffff";
+static const char gray4[]       = "#000000"; // #6d8dad
+static const char blue[]        = "#61afef";  // focused window border
+static const char green[]       = "#7EC7A2";
+static const char red[]         = "#e06c75";
+static const char orange[]      = "#caaa6a";
+static const char yellow[]      = "#EBCB8B";
+static const char pink[]        = "#c678dd";
+static const char col_borderbar[]  = "#1e222a"; // inner border
---
+++ chadwm/scripts/run.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+
+xrdb merge ~/.Xresources 
+xbacklight -set 10 &
+feh --bg-scale ~/Downloads/bg.jpg &
+xset r rate 200 50 &
+picom --experimental-backends &
+dwmbar &
+systemctl --user enable pulseaudio &
+systemctl --user start pulseaudio &
+while type chadwm >/dev/null; do chadwm && continue || break; done
